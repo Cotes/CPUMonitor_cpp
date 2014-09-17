303ff82dc08eb31c9e39e27b57bab3a5eafb9b78
Cotes <cristian.cotes@urv.cat>
2014-09-17 14:54:45 +0200

    Change tabs to spaces

--- CPUMonitor/CPUMonitor.cpp
+++ CPUMonitor/CPUMonitor.cpp
@@ -9,19 +9,19 @@ static void show_usage(std::string name)
               << "Options:\n"
               << "\t-h,--help\t\tShow this help message\n"
               << "\t-d,--duration <time_in_seconds>\tDuration of the monitoring in seconds\n"
-			  << "\t-i,--interval <time_in_milisecods>\tInterval between captures in ms. Min 50 ms\n"
-			  << "\t-o,--output <path>\tFile where with captured information\n"
+              << "\t-i,--interval <time_in_milisecods>\tInterval between captures in ms. Min 50 ms\n"
+              << "\t-o,--output <path>\tFile where with captured information\n"
               << std::endl;
 }
 
 int main(int argc, char* argv[])
 {
 
-	int option = 0;
-	int interval = -1, duration = -1;
-	std::string outputPath;
+    int option = 0;
+    int interval = -1, duration = -1;
+    std::string outputPath;
 
-	if (argc < 6) {
+    if (argc < 6) {
         show_usage(argv[0]);
         return 1;
     }
@@ -33,7 +33,7 @@ int main(int argc, char* argv[])
             return 0;
         } else if ((arg == "-d") || (arg == "--duraton")) {
             if (i + 1 < argc) { // Make sure we aren't at the end of argv!
-				i++;
+                i++;
                 duration = atoi(argv[i]);
             } else {
                   std::cerr << "--duration option requires one argument." << std::endl;
@@ -41,7 +41,7 @@ int main(int argc, char* argv[])
             }  
         } else if ((arg == "-i") || (arg == "--interval")) {
             if (i + 1 < argc) { // Make sure we aren't at the end of argv!
-				i++;
+                i++;
                 interval = atoi(argv[i]);
             } else {
                   std::cerr << "--interval option requires one argument." << std::endl;
@@ -49,7 +49,7 @@ int main(int argc, char* argv[])
             }  
         } else if ((arg == "-o") || (arg == "--output")) {
             if (i + 1 < argc) { // Make sure we aren't at the end of argv!
-				i++;
+                i++;
                 outputPath = argv[i];
             } else {
                   std::cerr << "--output option requires one argument." << std::endl;
@@ -58,54 +58,54 @@ int main(int argc, char* argv[])
         }
     }
 
-	std::vector<int> cpuValues;
+    std::vector<int> cpuValues;
 
-	duration *= 1000;
-	duration /= interval;
-	int i = 0;
-	while (i < duration) {
-		FILETIME old_idleTime;
-		FILETIME old_kernelTime;
-		FILETIME old_userTime;
-		BOOL res = GetSystemTimes( &old_idleTime, &old_kernelTime, &old_userTime );
-		Sleep(interval);
-		FILETIME idleTime;
-		FILETIME kernelTime;
-		FILETIME userTime;
-		res = GetSystemTimes( &idleTime, &kernelTime, &userTime );
+    duration *= 1000;
+    duration /= interval;
+    int i = 0;
+    while (i < duration) {
+        FILETIME old_idleTime;
+        FILETIME old_kernelTime;
+        FILETIME old_userTime;
+        BOOL res = GetSystemTimes( &old_idleTime, &old_kernelTime, &old_userTime );
+        Sleep(interval);
+        FILETIME idleTime;
+        FILETIME kernelTime;
+        FILETIME userTime;
+        res = GetSystemTimes( &idleTime, &kernelTime, &userTime );
 
-		ULARGE_INTEGER idleLow;
-		idleLow.QuadPart = idleTime.dwLowDateTime - old_idleTime.dwLowDateTime;
+        ULARGE_INTEGER idleLow;
+        idleLow.QuadPart = idleTime.dwLowDateTime - old_idleTime.dwLowDateTime;
 
-		ULARGE_INTEGER idleHigh;
-		idleHigh.QuadPart = idleTime.dwHighDateTime - old_idleTime.dwHighDateTime;
+        ULARGE_INTEGER idleHigh;
+        idleHigh.QuadPart = idleTime.dwHighDateTime - old_idleTime.dwHighDateTime;
 
-		ULARGE_INTEGER kernelLow;
-		kernelLow.QuadPart = kernelTime.dwLowDateTime - old_kernelTime.dwLowDateTime;
+        ULARGE_INTEGER kernelLow;
+        kernelLow.QuadPart = kernelTime.dwLowDateTime - old_kernelTime.dwLowDateTime;
 
-		ULARGE_INTEGER kernelHigh;
-		kernelHigh.QuadPart = kernelTime.dwHighDateTime - old_kernelTime.dwHighDateTime;
+        ULARGE_INTEGER kernelHigh;
+        kernelHigh.QuadPart = kernelTime.dwHighDateTime - old_kernelTime.dwHighDateTime;
 
-		ULARGE_INTEGER userLow;
-		userLow.QuadPart = userTime.dwLowDateTime - old_userTime.dwLowDateTime;
+        ULARGE_INTEGER userLow;
+        userLow.QuadPart = userTime.dwLowDateTime - old_userTime.dwLowDateTime;
 
-		ULARGE_INTEGER userHigh;
-		userHigh.QuadPart = userTime.dwHighDateTime - old_userTime.dwHighDateTime;
+        ULARGE_INTEGER userHigh;
+        userHigh.QuadPart = userTime.dwHighDateTime - old_userTime.dwHighDateTime;
 
-		int user = userLow.QuadPart;
-		int kernel = kernelLow.QuadPart;
-		int idle = idleLow.QuadPart;
-		int sys = user + kernel;
-		int cpuUse = (sys - idle)*100 / sys;
-		printf("CPU: %d\n", cpuUse);
-		cpuValues.push_back(cpuUse);
-		i++;
-	}
+        int user = userLow.QuadPart;
+        int kernel = kernelLow.QuadPart;
+        int idle = idleLow.QuadPart;
+        int sys = user + kernel;
+        int cpuUse = (sys - idle)*100 / sys;
+        //printf("CPU: %d\n", cpuUse);
+        cpuValues.push_back(cpuUse);
+        i++;
+    }
 
-	std::ofstream outputFile(outputPath);
+    std::ofstream outputFile(outputPath);
     std::ostream_iterator<int> outputIterator(outputFile, "\n");
     std::copy(cpuValues.begin(), cpuValues.end(), outputIterator);
 
-	return 0;
+    return 0;
 }
 
